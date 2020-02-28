c2c46f7ca2b7e8f2cdba03303769fa2e47188de1
kanhaiya <kanhaiyas@lambdatest.com>
2020-02-28 18:25:43 +0530

    wdio-lambdatest-service for webdriverio

---
+++ src/launcher.js
@@ -0,0 +1,112 @@
+import { performance, PerformanceObserver } from 'perf_hooks'
+import logger from '@wdio/logger'
+import LambdaTestTunnelLauncher from '@lambdatest/node-tunnel'
+import { TUNNEL_START_FAILED, TUNNEL_STOP_FAILED, TUNNEL_STOP_TIMEOUT } from './constants'
+const log = logger('@wdio/lambdatest-service')
+export default class LambdaTestLauncher {
+    constructor(options) {
+        this.options = options
+    }
+
+    // modify config and launch tunnel
+    onPrepare(config, capabilities) {
+        if (!this.options.tunnel) {
+            return
+        }
+
+        const tunnelArguments = {
+            user: config.user,
+            key: config.key,
+            ...this.options.lambdatestOpts
+        }
+
+        this.lambdatestTunnelProcess = new LambdaTestTunnelLauncher()
+
+        if (Array.isArray(capabilities)) {
+            capabilities.forEach(capability => {
+                capability.tunnel = true
+            })
+        } else if (typeof capabilities === 'object') {
+            capabilities.tunnel = true
+        }
+        // measure LT boot time
+        const obs = new PerformanceObserver(list => {
+            const entry = list.getEntries()[0]
+            log.info(
+                `LambdaTest Tunnel successfully started after ${entry.duration}ms`
+            )
+        })
+        obs.observe({ entryTypes: ['measure'], buffered: false })
+
+        let timer
+        performance.mark('ltTunnelStart')
+        return Promise.race([
+            new Promise((resolve, reject) => {
+                this.lambdatestTunnelProcess.start(tunnelArguments, err => {
+                    if (err) return reject(err)
+                    /* istanbul ignore next */
+                    this.lambdatestTunnelProcess.getTunnelName(tunnelName => {
+                        if (Array.isArray(capabilities)) {
+                            capabilities.forEach(capability => {
+                                capability.tunnelName = tunnelName
+                            })
+                        } else if (typeof capabilities === 'object') {
+                            capabilities.tunnelName = tunnelName
+                        }
+                        resolve()
+                    })
+                })
+            }),
+            new Promise((resolve, reject) => {
+                /* istanbul ignore next */
+                timer = setTimeout(() => { reject( new Error(TUNNEL_START_FAILED)) }, TUNNEL_STOP_TIMEOUT)
+            })
+        ]).then(
+            /* istanbul ignore next */
+            (result) => {
+                clearTimeout(timer)
+                performance.mark('ltTunnelEnd')
+                performance.measure('bootTime', 'ltTunnelStart', 'ltTunnelEnd')
+                return Promise.resolve(result)
+            },
+            (err) => {
+                clearTimeout(timer)
+                return Promise.reject(err)
+            }
+        )
+    }
+
+    onComplete() {
+        if (
+            !this.lambdatestTunnelProcess ||
+            typeof this.lambdatestTunnelProcess.isRunning !== 'function' ||
+            !this.lambdatestTunnelProcess.isRunning()
+        ) {
+            return
+        }
+
+        let timer
+        return Promise.race([
+            new Promise((resolve, reject) => {
+                this.lambdatestTunnelProcess.stop(err => {
+                    if (err) return reject(err)
+                    resolve()
+                })
+            }),
+            new Promise((resolve, reject) => {
+                /* istanbul ignore next */
+                timer = setTimeout(() => reject( new Error(TUNNEL_STOP_FAILED)), TUNNEL_STOP_TIMEOUT)
+            })
+        ]).then(
+            () => {
+                clearTimeout(timer)
+                return Promise.resolve()
+            },
+            /* istanbul ignore next */
+            (err) => {
+                clearTimeout(timer)
+                return Promise.reject(err)
+            }
+        )
+    }
+}
---
+++ tests/service.test.js
@@ -0,0 +1,207 @@
+import LambdaTestService from '../src'
+const uri = '/some/uri'
+global.browser = {
+    config: {},
+    execute: jest.fn(),
+    chromeA: { sessionId: 'sessionChromeA' },
+    chromeB: { sessionId: 'sessionChromeB' },
+    chromeC: { sessionId: 'sessionChromeC' },
+    instances: ['chromeA', 'chromeB', 'chromeC']
+}
+test('beforeSuite', () => {
+    const service = new LambdaTestService()
+    expect(service.suiteTitle).toBeUndefined()
+    service.beforeSuite({ title: 'foobar' })
+    expect(service.suiteTitle).toBe('foobar')
+})
+
+test('beforeSession should set to unknown creds if no lambdatest user and key are found', () => {
+    const service = new LambdaTestService()
+    const config = {}
+    service.beforeSession(config, {})
+    expect(service.isServiceEnabled).toBe(false)
+})
+
+test('afterSuite', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({}, {})
+
+    expect(service.failures).toBe(0)
+
+    service.afterSuite({})
+    expect(service.failures).toBe(0)
+})
+
+test('beforeTest', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.beforeTest({
+        fullName: 'foobar',
+        parent: 'Jasmine__TopLevel__Suite'
+    })
+    expect(service.suiteTitle).toBeUndefined()
+})
+
+test('beforeTest', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+
+    service.suiteTitle = 'Jasmine__TopLevel__Suite'
+    service.beforeTest({
+        fullName: 'foobar',
+        parent: 'Jasmine__TopLevel__Suite'
+    })
+    expect(service.suiteTitle).toBeDefined()
+})
+
+test('beforeTest', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.isServiceEnabled = false
+    service.beforeTest({
+        fullName: 'foobar',
+        parent: 'Jasmine__TopLevel__Suite'
+    })
+    expect(service.suiteTitle).toBeUndefined()
+})
+
+test('afterTest', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({}, {})
+
+    expect(service.failures).toBe(0)
+
+    service.afterTest({ passed: true })
+    expect(service.failures).toBe(0)
+
+    service.afterTest({ passed: false })
+    expect(service.failures).toBe(1)
+})
+
+test('after', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = false
+    global.browser.sessionId = 'foobar'
+    service.after()
+
+    expect(service.updateJob).toBeCalledWith('foobar', 5)
+})
+
+test('after', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = false
+    global.browser.sessionId = 'foobar'
+    global.browser.config = { mochaOpts: { bail: 1 } }
+    service.after(1)
+
+    expect(service.updateJob).toBeCalledWith('foobar', 1)
+})
+
+test('afterScenario', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({}, {})
+
+    expect(service.failures).toBe(0)
+
+    service.afterScenario(uri, {}, {}, { status: 'passed' })
+    expect(service.failures).toBe(0)
+
+    service.afterScenario(uri, {}, {}, { status: 'failed' })
+    expect(service.failures).toBe(1)
+
+    service.afterScenario(uri, {}, {}, { status: 'passed' })
+    expect(service.failures).toBe(1)
+
+    service.afterScenario(uri, {}, {}, { status: 'failed' })
+    expect(service.failures).toBe(2)
+})
+
+test('after with bail set', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = false
+    global.browser.sessionId = 'foobar'
+    global.browser.config = { mochaOpts: { bail: 1 } }
+    service.after(1)
+
+    expect(service.updateJob).toBeCalledWith('foobar', 1)
+})
+
+test('after in multiremote', () => {
+    const service = new LambdaTestService()
+    service.beforeSession(
+        { user: 'foobar', key: '123' },
+        { chromeA: {}, chromeB: {}, chromeC: {} }
+    )
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = true
+    global.browser.sessionId = 'foobar'
+    service.after()
+
+    expect(service.updateJob).toBeCalledWith(
+        'sessionChromeA',
+        5,
+        false,
+        'chromeA'
+    )
+    expect(service.updateJob).toBeCalledWith(
+        'sessionChromeB',
+        5,
+        false,
+        'chromeB'
+    )
+    expect(service.updateJob).toBeCalledWith(
+        'sessionChromeC',
+        5,
+        false,
+        'chromeC'
+    )
+})
+
+test('onReload', () => {
+    const service = new LambdaTestService()
+    service.beforeSession({ user: 'foobar', key: '123' }, {})
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = false
+    global.browser.sessionId = 'foobar'
+    service.onReload('oldbar', 'newbar')
+
+    expect(service.updateJob).toBeCalledWith('oldbar', 5, true)
+})
+
+test('after in multiremote', () => {
+    const service = new LambdaTestService()
+    service.beforeSession(
+        { user: 'foobar', key: '123' },
+        { chromeA: {}, chromeB: {}, chromeC: {} }
+    )
+    service.failures = 5
+    service.updateJob = jest.fn()
+
+    global.browser.isMultiremote = true
+    global.browser.sessionId = 'foobar'
+    global.browser.chromeB.sessionId = 'newSessionChromeB'
+    service.onReload('sessionChromeB', 'newSessionChromeB')
+
+    expect(service.updateJob).toBeCalledWith(
+        'sessionChromeB',
+        5,
+        true,
+        'chromeB'
+    )
+})
