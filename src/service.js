0c978a61f60c2c9f5d017e1bb4c1aa7fead3bc58
Shahnawaz-LambdaTest <shahnawaz@lambdatest.com>
2024-06-24 22:03:02 +0000

    changes for logging session URL

--- src/service.js
+++ src/service.js
@@ -241,11 +241,21 @@ export default class LambdaRestService {
 
     if (!this._browser.isMultiremote) {
       log.info(`Update job with sessionId ${this._browser.sessionId}, ${status}`);
+
+      // Print session URL for single remote browser
+      const sessionURL = this.getSessionURL(this._browser.sessionId, this._config.product);
+      log.info(`Session URL: ${sessionURL}`);
+
       return this._update({ sessionId: this._browser.sessionId, failures: result });
     }
 
     return Promise.all(Object.keys(this._capabilities).map(browserName => {
       log.info(`Update multiremote job for browser '${browserName}' and sessionId ${this._browser[browserName].sessionId}, ${status}`);
+
+      // Print session URL for each remote browser
+      const sessionURL = this.getSessionURL(this._browser[browserName].sessionId, this._config.product);
+      log.info(`Session URL for ${browserName}: ${sessionURL}`);
+
       return this._update({ sessionId: this._browser[browserName].sessionId, failures: failures, calledOnReload: false, browserName: browserName });
     }));
   }
@@ -260,11 +270,20 @@ export default class LambdaRestService {
     if (!this._browser.isMultiremote) {
       log.info(`Update (reloaded) job with sessionId ${oldSessionId}, ${status}`);
 
+      // Print session URL for single remote browser
+      const sessionURL = this.getSessionURL(this._browser.sessionId, this._config.product);
+      log.info(`Session URL: ${sessionURL}`);
+
       await this._update({ sessionId: oldSessionId, fullTitle: this._currentTestTitle, status: status, calledOnReload: true });
 
     } else {
       const browserName = this._browser.instances.filter(browserName => this._browser[browserName].sessionId === newSessionId)[0];
       log.info(`Update (reloaded) multiremote job for browser '${browserName}' and sessionId ${oldSessionId}, ${status}`);
+
+      // Print session URL for each remote browser
+      const sessionURL = this.getSessionURL(this._browser[browserName].sessionId, this._config.product);
+      log.info(`Session URL for ${browserName}: ${sessionURL}`);
+
       await this._update({ sessionId : oldSessionId, failures:this._failures, calledOnReload: true, browserName: browserName });
     }
 
@@ -392,5 +411,12 @@ export default class LambdaRestService {
       }));
     }
     return await this._browser.execute(cmd);
+  }
+
+  getSessionURL(sessionId, product) {
+    if (product === 'appAutomation') {
+      return `https://appautomation.lambdatest.com/test?testID=${sessionId}`;
+    }
+    return `https://automation.lambdatest.com/test?testID=${sessionId}`;
   }
 }
